f7f5d4a6e045d23369f46142533d7604b6033110
pauljay <pauljay611@gmail.com>
2018-06-02 15:38:48 +0800

    second commit

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,10 @@
+const sha256 = require('sha256');
+
 function Blockchain(){
     this.chain = [];
     this.pendingTransactions = [];
+    
+    this.createNewBlock(100, '0', '0');
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
@@ -31,6 +35,22 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recipient){
     };
     this.pendingTransactions.push(newTransaction);
     return this.getLastBlock()['index']+1;
+}
+
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce){
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataAsString);
+    return hash;
+}
+
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData){
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    while(hash.substring(0,4)!=='0000'){
+        nonce++;
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    }
+    return nonce;
 }
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -2,18 +2,5 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2389564,'0INA90SDNF90N', '90ADSFHDFHSDW');
-
-bitcoin.createNewTransaction(0.002, '0xsaasfasfdads', 'NUUASDASDASD');
-
-bitcoin.createNewBlock(1245678,'0Igbgn90SDNF90N', 'DVOBKCMBOMSGO');
-
-bitcoin.createNewTransaction(0.002, '0xsaasfasfdads', 'NUUASDASDASD');
-bitcoin.createNewTransaction(0.1, '0EEESDFXBCs', 'NUXCVBXCVBXCB');
-bitcoin.createNewTransaction(0.5, '0SDFSDFSDFasfdads', 'NUSDFSFSFSFSD');
-
-bitcoin.createNewBlock(367448,'ASDFASDFASDFAASDF', 'ASDGADFAGSADF');
-
-
 console.log(bitcoin);
 
